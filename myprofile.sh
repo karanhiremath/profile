e1eff156772d82558b3de8baf8bba5a28c07454b
Karan Hiremath <karanhir@seas.upenn.edu>
2015-10-06 14:26:41 -0400

    changing to only show branch when neccessary

--- myprofile.sh
+++ myprofile.sh
@@ -8,36 +8,31 @@ if [ -f ~/profile/git-completion.bash ]; then
   source ~/profile/git-completion.bash
 fi
 
-
-function git_color {
-  local git_status="$(git status 2> /dev/null)"
-
-  if [[ ! $git_status =~ "working directory clean" ]]; then
-    echo -e $(tput setaf 9)
-  elif [[ $git_status =~ "Your branch is ahead of" ]]; then
-    echo -e $(tput setaf 11)
-  elif [[ $git_status =~ "nothing to commit" ]]; then
-    echo -e $(tput setaf 12)
-  else
-    echo -e $(tput setaf 12)
-  fi
-}
-
 function git_branch {
   local git_status="$(git status 2> /dev/null)"
   local on_branch="On branch ([^${IFS}]*)"
   local on_commit="HEAD detached at ([^${IFS}]*)"
 
+  if [[ ! $git_status =~ "working directory clean" ]]; then
+    local git_color="$(tput setaf 9)"
+  elif [[ $git_status =~ "Your branch is ahead of" ]]; then
+    local git_color="$(tput setaf 11)"
+  elif [[ $git_status =~ "nothing to commit" ]]; then
+    local git_color="$(tput setaf 12)"
+  else
+    local git_color="$(tput setaf 12)"
+  fi
+
   if [[ $git_status =~ $on_branch ]]; then
     local branch=${BASH_REMATCH[1]}
-    echo " ($branch)"
+    echo " $(tput setaf 11)|$(git_color) ($branch)"
   elif [[ $git_status =~ $on_commit ]]; then
     local commit=${BASH_REMATCH[1]}
-    echo " ($commit)"
+    echo " $(tput setaf 11)| $(git_color) ($commit)"
   fi
 }
 
-export PROMPT_COMMAND='PS1="\[$(tput bold)\]\[$(tput setaf 9)\]\T \[$(tput setaf 11)\]| \[$(tput setaf 12)\]\u\[$(tput setaf 9)\]@\h \[$(tput setaf 11)\]| \[$(tput setaf 9)\]\w \[$(tput setaf 11)\]|\[\$(git_color)\]\$(git_branch) \[$(tput setaf 11)\]: \[$(tput sgr0)\]"'
+export PROMPT_COMMAND='PS1="\[$(tput bold)\]\[$(tput setaf 9)\]\T \[$(tput setaf 11)\]| \[$(tput setaf 12)\]\u\[$(tput setaf 9)\]@\h \[$(tput setaf 11)\]| \[$(tput setaf 9)\]\W\$(git_branch) \[$(tput setaf 11)\]: \[$(tput sgr0)\]\[$(tput bold)\]"'
 
 export CLICOLOR=1$Color_Off
 export LSCOLORS=ExFxCxDxBxEGEDABAGACAD
@@ -56,3 +51,5 @@ gacp ()
 }
 
 alias es='ssh karan@10.0.1.90'
+
+alias npm='sudo npm'
