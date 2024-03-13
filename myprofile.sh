89b9103dbd87ce459d720792e12981570edc5f2a
Karan Hiremath <karan@easypost.com>
2024-03-13 11:56:06 -0700

    saving changes from macbook that are ancient

--- myprofile.sh
+++ myprofile.sh
@@ -6,7 +6,7 @@ alias tmux="tmux -2"
 
 git config --global user.email 'me@karanhiremath.com'
 git config --global user.name 'Karan Hiremath'
-git config --global --add --bool push.autoSetupRemote true
+git config --global --bool push.autoSetupRemote true
 git config --global pull.rebase true
 
 
